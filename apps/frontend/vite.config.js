b5f71501ac591ced8b1e0e7334e6e68c631dcaab
Esteban1543 <jhoanestebanpv1@gmail.com>
2025-02-12 22:35:17 -0500

    Add allowed hosts configuration to Vite server

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4001
+    port: 4001,
+    allowedHosts: [
+      'facturacion.ddneprojects.online'
+    ]
   }
 })
